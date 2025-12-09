a60a9db1ce613430fee82fe75f55c36a5b23387b
Sandeep Sarkar <sandeeps0774@gmail.com>
2025-12-09 18:38:34 +0530

    Add files via upload

---
+++ project/Backend/app.js
@@ -0,0 +1,24 @@
+const express=require("express")
+const app=express();
+const authRoutes = require('./routes/userauth');
+const farmerRoutes = require('./routes/usercrud');
+const pesticideRoutes = require('./routes/pesticideRoutes');
+const nutrientsRoutes =require('./routes/nutrientsroute.js');
+const insuranceRoutes = require('./routes/insuranceRoutes');
+
+
+require("dotenv").config();
+require("./connection/conn.js")
+const cors = require('cors');
+app.use(express.json());
+//creating the port of the app
+app.use(cors());
+app.use('/api/auth', authRoutes);
+app.use('/api/nutrients', nutrientsRoutes);
+app.use('/api/insurance', insuranceRoutes)
+
+app.use('/api/farmers', farmerRoutes);
+app.use('/api/pesticides', pesticideRoutes);
+app.listen(process.env.PORT,()=>{
+    console.log(`SERVER STARTED at port ${process.env.PORT}`)
+})
